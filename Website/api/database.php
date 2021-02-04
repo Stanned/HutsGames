6300d987fed41530af0d63b09eb852dafa45473f
Stan Kemp <stanexe1717@gmail.com>
2021-02-04 20:19:36 +0100

    Updates met database connection. database.php nu zo goed als klaar.

--- Website/api/database.php
+++ Website/api/database.php
@@ -1,14 +1,20 @@
 <?php
 
-$servername = "mysql.stackcp.com";
-$port = "53704";
-$username = "HutsGames-313537cd22";
-$password = "chx5p0qwvd";
-$dbname = "HutsGames-313537cd22";
+class Database{
 
-$conn = new mysqli($servername, $username, $password, $dbname, $port);
+    private string $host = "mysql.stackcp.com";
+    private string $port = "53704";
+    private string $username = "HutsGames-313537cd22";
+    private string $database = "HutsGames-313537cd22";
+    private string $password = "chx5p0qwvd";
 
-if ($conn->connect_error) {
-    die("Connection to database has failed: " . $conn->connect_error);
+    public function getDbConnection(): mysqli
+    {
+        $conn = new mysqli($this->host, $this->username, $this->password, $this->database, $this->port);
+
+        if ($conn->connect_error) {
+            echo("Error in connection: " . mysqli_connect_error());
+        }
+        return $conn;
+    }
 }
-echo "Connected to database.";
--- Website/api/register.php
+++ Website/api/register.php
@@ -1,1 +1,18 @@
 <?php
+
+include './database.php';
+
+$database = new Database();
+$conn = $database->getDbConnection();
+
+if ($conn) {
+    if($result = $conn->query("SELECT * FROM users")) {
+        while($row = $result->fetch_assoc()) {
+            echo(json_encode($row));
+        }
+    }
+    echo("<br>Success");
+
+} else {
+    echo("error");
+}
