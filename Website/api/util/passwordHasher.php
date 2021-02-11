348850a24aaba32e96cc35618ff66b3ad54d2bf6
Stan Kemp <stanexe1717@gmail.com>
2021-02-11 17:13:06 +0100

    Removed basically all functions because apparently php already salts passwords.

--- Website/api/util/passwordHasher.php
+++ Website/api/util/passwordHasher.php
@@ -3,30 +3,9 @@
 class passwordHasher
 {
     // Function for generating without a salt
-    public function hashPassword(string $password): array
+    public function hashPassword(string $password): string
     {
-        $salt = $this->generateRandomString();
-        $saltedPass = $password . $salt;
-        return array(password_hash($saltedPass, PASSWORD_DEFAULT), $salt);
-    }
-
-    private function generateRandomString(): string
-    {
-        $length = 16;
-        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        $charactersLength = strlen($characters);
-        $randomString = '';
-        for ($i = 0; $i < $length; $i++) {
-            $randomString .= $characters[rand(0, $charactersLength - 1)];
-        }
-        return $randomString;
-    }
-
-    // Function for generating hash with a known salt
-    public function hashPasswordWithSalt(string $password, string $salt): string
-    {
-        $saltedPass = $password . $salt;
-        return password_hash($saltedPass, PASSWORD_DEFAULT);
+        return password_hash($password, PASSWORD_DEFAULT);
     }
 }
 
