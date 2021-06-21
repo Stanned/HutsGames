dd185b38c151202974eb9803cc54fdafff738ef7
mikalittt <61689235+Mikaiscool@users.noreply.github.com>
2021-06-21 08:08:45 +0200

    Kleine aanpassingen

--- .idea/contact/contact.php
+++ .idea/contact/contact.php
@@ -10,6 +10,8 @@ if(!empty($_POST["submitContactForm"])) {
     $senderEmail=$_POST["senderEmail"];
     $subject=$_POST["subject"];
     $message=$_POST["message"];
+    //Op deze manier wordt de body van de mail HTML
+    $headers2 .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
     
     //Laat zien bij beide emails waar hij vandaan komt. PErsoneel van HutsGames ziet dus naam bij afzender en customer@sender.com. De klant ziet HutsGames staan met noreply@hutsgames.com. Dit is een verzonnen emailadres.
     $headers1 .= "From: $sender <customer@sender.com>\r\n";
@@ -17,7 +19,7 @@ if(!empty($_POST["submitContactForm"])) {
 
     //Er worden twee verschillende emails opgesteld.
     $mailBody1="Contact Form Message\n\nName: $sender\nE-mail: $senderEmail\n\n$message";
-    $mailBody2="Here is your message sent to us:\n\n$message\n\nDo not reply to this automated message, we will contact you soon!\n\nSincerely,\nHutsGames";
+    $mailBody2="Here is your message sent to us:</br></br><i>$message</i></br></br>Do not reply to this automated message, we will contact you soon!</br></br>Sincerely,</br>HutsGames";
 
     mail($website, $subject, $mailBody1, $headers1);
     mail($customer, $subject, $mailBody2, $headers2);
@@ -161,11 +163,11 @@ function checkMessage() {
     var message = document.forms["myForm"]["messageInput"].value;
     var messageNum = message.replace(/[a-z]/, '');
     
-    if (messageNum.length > 0 && messageNum.length < 301) {
+    if (messageNum.length > 0 && messageNum.length < 1001) {
         document.getElementById("messageInput").className = 'InputCorrect';
         return true;
     } 
-    else if (messageNum.length < 1 || messageNum.length > 300) {
+    else if (messageNum.length < 1 || messageNum.length > 1000) {
         document.getElementById("messageInput").className = 'InputIncorrect';
         return false;
     }
