4a641fafd9cf73c3907f99248666ab674cabaf71
Stan Kemp <stanexe1717@gmail.com>
2021-06-25 00:29:40 +0200

    db ww pt.2

--- Website/api/util/database.php
+++ Website/api/util/database.php
@@ -2,14 +2,13 @@
 class Database{
 
     private string $host = "localhost";
-    private string $port = "53704";
     private string $username = "hutsgames";
     private string $database = "hutsgames";
     private string $password = "superhuts123";
 
     public function getDbConnection(): PDO
     {
-        $dsn = "mysql:host=$this->host;dbname=$this->database;port=$this->port";
+        $dsn = "mysql:host=$this->host;dbname=$this->database";
         try {
             $conn = new PDO($dsn, $this->username, $this->password);
         } catch (PDOException $e) {
