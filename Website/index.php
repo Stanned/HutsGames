5876ecc14145bd8182adca64281c10230412a5e0
LuukDriessen <77145153+LuukDriessen@users.noreply.github.com>
2021-06-25 22:22:07 +0200

    submit

--- Website/index.php
+++ Website/index.php
@@ -130,7 +130,16 @@
             }
         }
 
+          function setComments() {
+            if(isset($_POST['commentSubmit'])) {
+              $uid = $_POST['uid'];
+              $date = $_POST ['date'];
+              $message = $_POST['message'];
 
+              $sql = "INSERT INTO comments (uid, date, message) VALUES ('$uid', '$date', '$message')";
+              $result = $conn->query($sql);
+            }
+          }
 
         ?>
     </footer>
